ebeb4e103a244251a28401f9d03ff18ef10e2a1f
Alexander Zaytsev <alexanderzaitsev89@gmail.com>
2016-11-24 22:39:43 +0200

    New stuff added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-"# GalleryForSculptur" 
+"Project made for sculptur Pavel Kaltygin www.pavelkaltygin.com" 
+
+
+Project uses:
+     Bootstrap template:
+## Copyright and License
+Copyright 2013-2015 Iron Summit Media Strategies, LLC. Code released under the [Apache 2.0](https://github.com/IronSummitMedia/startbootstrap-grayscale/blob/gh-pages/LICENSE) license.
+     Magnific Popup:
+Plagin made by : http://dimsemenov.com/plugins/magnific-popup/
--- pavel/js/lightboxMagnificent2.js
+++ pavel/js/lightboxMagnificent2.js
@@ -3,29 +3,59 @@ $('.magnific-gallery').each(function(index, value) {
   var title='wtf';
   switch (index) {
     case 0:
-      console.log("was here");
+      //console.log("was here");
       title="Source";
       break;
     case 1:
-      title="Saint Christopher";
+      title="Ilmarinen";
       break;
     case 2:
-      title="Harlequin";
+      title="Monumentalist";
     case 3:
-      title="David";
+      title="Donkey";
       break;
     case 4:
-      title="Temptation of Pandora";
+      title="Sunrise";
       break;
+      case 5:
+          title="Ferdinand Porsche";
+          break;
+      case 6:
+          title="Harlequin";
+          break;
+      case 7:
+          title="Harvest Festival";
+          break;
+      case 8:
+          title="Otter";
+          break;
+      case 9:
+          title="Messenger";
+          break;
+      case 10:
+          title="The Maiden of Pohjola";
+          break;
+      case 11:
+        title="David";
+        break;
+      case 12:
+          title="Wounded Hare";
+          break;
+      case 13:
+          title="Equilibrium";
+          break;
+      case 14:
+          title="The Temptation of Pandora";
+          break;
+      case 15:
+          title="Saint Christopher";
+          break;
+
     default:
       //Statements executed when none of the values match the value of the expression
       break;
   }
   var gallery = $(this);
-
-  if(index==0){
-    console.log("wtf");
-  }
   var galleryImages = $(this).data('links').split(',');
   var items = [];
   for (var i = 0; i < galleryImages.length; i++) {
